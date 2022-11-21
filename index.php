23c6b74c77994889eb223bbe0a8f38128add8739
Sofya033 <113354571+Sofya033@users.noreply.github.com>
2022-11-21 18:08:03 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php
+
+require_once "./NewsClass.php";
+require_once "./AdvertClass.php";
+require_once "./ContentClass.php";
+
+$production[] = new NewsClass();
+$production[] = new AdvertClass();
+$production[] = new ContentClass();
+
+foreach ($production as $item) {
+	if ($item instanceof AbstractProductionClass) {
+		$item->doPrint();
+	}else {
+		echo "К Production не относится";
+	}
+}
